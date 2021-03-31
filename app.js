6f3102d19288471e249ee7ba38bfbfe2c0f0e4c5
Ace1862020 <kyds1862015@gmail.com>
2021-03-31 13:04:31 +0800

    add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const db = require('./models')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -12,11 +13,14 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'forumSecret', resave: false, saveUninitialized: false }))
-app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_message = req.flash('success_message')
-  res.locals.error_message = req.flash('error_message')
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   next()
 })
 
@@ -24,6 +28,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,13 +9,13 @@ const userController = {
 
   signUp: (req, res) => {
     if (req.body.password !== req.body.passwordCheck) {
-      req.flash('error_message', '密碼與密碼確認不符!')
+      req.flash('error_messages', '密碼與密碼確認不符!')
       return res.redirect('/signup')
     } else {
       User.findOne({ where: { email: req.body.email } })
         .then(user => {
           if (user) {
-            req.flash('error_message', 'Email 已被註冊!')
+            req.flash('error_messages', 'Email 已被註冊!')
             return res.redirect('/signup')
           } else {
             User.create({
@@ -23,13 +23,29 @@ const userController = {
               email: req.body.email,
               password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
             }).then(user => {
-              req.flash('success_message', '註冊成功')
+              req.flash('success_messages', '註冊成功')
               return res.redirect('/signin')
             })
           }
         })
     }
+  },
+
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+
+  signIn: (req, res) => {
+    req.flash('success_messages', '登入成功！')
+    res.redirect('/restaurants')
+  },
+
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
+
 }
 
 module.exports = userController
