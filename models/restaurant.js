8f38db67d34f9eb48bdf15df7559c415a96bff70
Ace1862020 <kyds1862015@gmail.com>
2021-04-07 10:37:46 +0800

    Create comment model & add association among users, comment, restaurants

--- models/restaurant.js
+++ models/restaurant.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Restaurant.belongsTo(models.Category)
+      Restaurant.hanMany(models.Comment)
     }
   };
   Restaurant.init({
