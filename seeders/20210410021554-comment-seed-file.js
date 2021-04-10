5f89015be5ff9e48c68e32d4ddb336567b2d9e2c
Ace1862020 <kyds1862015@gmail.com>
2021-04-10 17:27:25 +0800

    Add comment seed file

---
+++ seeders/20210410021554-comment-seed-file.js
@@ -0,0 +1,28 @@
+'use strict';
+const faker = require('faker');
+const db = require('../models')
+const User = db.User
+const Restaurant = db.Restaurant
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await User.findAll()
+    const restaurants = await Restaurant.findAll()
+
+    await queryInterface.bulkInsert('Comments',
+      Array.from({ length: 5 }).map((comment, index) =>
+      ({
+        id: index + 1,
+        text: faker.lorem.sentence(),
+        UserId: users[Math.floor(Math.random() * 3)].id,
+        RestaurantId: restaurants[Math.floor(Math.random() * 50)].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Comments', null, {})
+  }
+};
