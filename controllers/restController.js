31b51948aa9e403cd56908bb2e12de116f082b86
Ace1862020 <kyds1862015@gmail.com>
2021-04-11 15:03:45 +0800

    Add media attention feature & dashboard page

--- controllers/restController.js
+++ controllers/restController.js
@@ -90,6 +90,18 @@ const restController = {
     })
   },
 
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then((restaurant) => {
+      console.log('restaurant.Comment:', restaurant.Comment)
+      return res.render('dashboard', { restaurant: restaurant.toJSON() })
+    })
+  },
+
 }
 
 module.exports = restController
